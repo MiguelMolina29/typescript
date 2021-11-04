6ab561f179d4073285b418a6347915796609d713
MiguelMolina29 <miguelantonio.molinago@my.uvm.edu.mx>
2021-11-04 17:05:51 -0600

    strings

---
+++ tipos/strings.js
@@ -0,0 +1,10 @@
+"use strict";
+(function () {
+    var _a;
+    var batman = 'batman';
+    var linternaVerde = "Linterna Verde";
+    console.log(batman.toUpperCase());
+    console.log(((_a = batman[10]) === null || _a === void 0 ? void 0 : _a.toLocaleLowerCase()) || 'No está ');
+    // Dependiendo el "?" podemos usar el operador ternario o solo verlo
+    // Como una condicional para undefined
+})();
---
+++ tipos/strings.ts
@@ -0,0 +1,12 @@
+(() => {
+
+    const batman: string = 'batman';
+    const linternaVerde: string = "Linterna Verde";
+
+    console.log(batman.toUpperCase());
+
+    console.log(batman[10]?.toLocaleLowerCase() || 'No está ');
+    // Dependiendo el "?" podemos usar el operador ternario o solo verlo
+    // Como una condicional para undefined
+
+})()
